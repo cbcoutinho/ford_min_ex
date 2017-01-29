e8467c5c8fb1b97499685ef6ed416eb43fe79162
Chris Coutinho <chrisbcoutinho@gmail.com>
2017-01-30 00:21:57 +0100

    Edit script

--- script.py
+++ script.py
@@ -6,3 +6,8 @@ if proj_data['relative']:
     proj_data['project_url'] = '.'
 
 project = ford.fortran_project.Project(proj_data)
+
+if proj_data['relative']:
+    project.markdown(md,'..')
+else:
+    project.markdown(md,proj_data['project_url'])
